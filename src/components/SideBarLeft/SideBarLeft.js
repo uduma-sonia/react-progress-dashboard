b2220daa7e310256b6763b9f30f03687fe1335fe
uduma sonia <sohnyauduma@gmail.com>
2022-01-17 01:23:39 +0100

    feat: side bar left

--- src/components/SideBarLeft/SideBarLeft.js
+++ src/components/SideBarLeft/SideBarLeft.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSlidersH, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSlidersH,
+  faCubes,
+  faWrench,
+} from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 function SideBar() {
   return (
     <div className="flex flex-col items-center bg-slate-100 pt-10 w-15 h-screen">
       <button className="logo">P</button>
 
-      <div className="border border-green-800 mt-32 flex flex-col">
-        <FontAwesomeIcon className="text-slate-500" icon={faSlidersH} />
-        <FontAwesomeIcon className="text-slate-500" icon={faUser} />
-        <p>B</p>
-        <p>C</p>
-        <p>D</p>
+      <div className="mt-32 flex flex-col justify-between h-72">
+        <FontAwesomeIcon
+          className="text-slate-500"
+          icon={faSlidersH}
+          size="lg"
+        />
+        <FontAwesomeIcon className="text-slate-500" icon={faCubes} size="lg" />
+        <FontAwesomeIcon className="text-slate-500" icon={faUser} size="lg" />
+        <FontAwesomeIcon className="text-slate-500" icon={faWrench} size="lg" />
       </div>
     </div>
   );
